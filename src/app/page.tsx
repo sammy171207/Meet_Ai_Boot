05484e1fd0504b15863cc2b300c07e47ab4c3787
sammy171207 <sameerrandive171207@gmail.com>
2025-08-31 18:41:46 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      <Button variant={"outline"}>Hello World</Button>
+    </div>
+  )
+}
+
+export default Home
