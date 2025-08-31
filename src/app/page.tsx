fcb7e52555e7b196e4a123d9deee64ab1c69d958
sammy171207 <sameerrandive171207@gmail.com>
2025-08-31 23:46:39 +0530

    better_auth_setupdone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
+
+'use client'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Input } from '@/components/ui/input';
+import { authClient } from '@/lib/auth-client';
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [name,setName]=useState<string>("");
+  const [email,setEmail]=useState<string>('');
+  const [password,setPassword]=useState<string>('');
+ 
+
+  const handleSubmit=async(e:React.FormEvent<HTMLButtonElement>)=>{
+    e.preventDefault();
+    console.log(name,email,password);
+    await authClient.signUp.email({name,email,password},{
+      onSuccess: (data) => {
+        console.log(data)
+        alert('success')
+      },
+      onError: (error) => {
+        console.log(error)
+      }
+    })
+    alert('success')
+  }
+
   return (
     <div className='text-3xl font-bold underline'>
-      <Button variant={"outline"}>Hello World</Button>
+      <Input type='text' placeholder='name' value={name} onChange={(e)=>setName(e.target.value)}/>
+      <Input type='email' placeholder='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <Input type='password' placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      <Button  onClick={handleSubmit}>Submit</Button>
     </div>
   )
 }
